bff25e81c40e3b862073c296f4be6e1443810288
LuMengli <mengfaluo@163.com>
2018-04-26 09:06:59 +0800

    modified login-router

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ export const constantRouterMap = [
   {path: '/404', component: _import('errorPage/404'), hidden: true},
   {
     path: '/',
-    redirect: '/integrate-monitor/overview',
+    redirect: '/login',
     hidden: true
   },
   {
--- src/utils/index.js
+++ src/utils/index.js
@@ -80,11 +80,11 @@ export function getQueryObject(url) {
  */
 export function getByteLen(val) {
   let len = 0
-  for (let i = 0; i < val.length; i++) {
-    if (val[i].match(/[^\x00-\xff]/ig) != null) {
-      len += 1
-    } else { len += 0.5 }
-  }
+  // for (let i = 0; i < val.length; i++) {
+  //   if (val[i].match(/[^\x00-\xff]/ig) != null) {
+  //     len += 1
+  //   } else { len += 0.5 }
+  // }
   return Math.floor(len)
 }
 
