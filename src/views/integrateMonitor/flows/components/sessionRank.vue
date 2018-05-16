c5e3e9c482a51de4aa18dc10d588369902d91c0f
XWindBell <xhuan107@163.com>
2018-05-16 17:56:26 +0800

    继续修改integrateMonitor下的flows页面

---
+++ src/views/integrateMonitor/flows/components/sessionRank.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-table :data="dataList" border style="width: 100%" height="250">
+    <el-table-column
+      label="源(名称)"
+      prop="srcName"
+      header-align="center"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      label="源(地址)"
+      prop="srcAdd"
+      header-align="center"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      label="目的地(名称)"
+      prop="desName"
+      header-align="center"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      label="目的地(地址)"
+      prop="desAdd"
+      header-align="center"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      label="应用协议"
+      prop="appPro"
+      header-align="center"
+      align="center">
+    </el-table-column>
+    <el-table-column
+      label="流量"
+      prop="flow"
+      sortable
+      header-align="center"
+      align="center">
+    </el-table-column>
+  </el-table>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      dataList: Array
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
--- src/views/integrateMonitor/flows/flows.vue
+++ src/views/integrateMonitor/flows/flows.vue
@@ -8,7 +8,7 @@
       </el-row>
     </div>
     <div class="chart-wrapper">
-      <el-row>
+      <el-row :gutter="60">
         <el-col :xs="24" :sm="24" :lg="12">
           <appTranStat title="Top 5应用层/传输层协议统计" id="appTranStat"></appTranStat>
         </el-col>
@@ -17,29 +17,68 @@
         </el-col>
       </el-row>
     </div>
+    <div class="table-wrapper">
+      <el-row>
+        <el-col :xs="24" :sm="24" :lg="24">
+          <TableWrapper title="Top 20会话排行" wrapperHeight="420px">
+            <sessionRank :dataList="sessRankData"></sessionRank>
+            <!--分页-->
+            <el-pagination style="margin-top: 20px;margin-left: 35%;"
+                           background
+                           layout="prev, pager, next"
+                           :total="130">
+            </el-pagination>
+          </TableWrapper>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 <script type="text/ecmascript-6">
-  // import axios from 'axios'
   import IPStat from './components/IPStat'
   import appTranStat from './components/appTranStat'
   import portStat from './components/PortStat'
+  import sessionRank from './components/sessionRank'
+  import TableWrapper from 'components/table/tableWrapper'
+  import axios from 'axios'
   export default {
     components: {
       IPStat,
       appTranStat,
-      portStat
+      portStat,
+      TableWrapper,
+      sessionRank
     },
     data() {
-      return {}
+      return {
+        sessRankData: []
+      }
     },
-    methods: {},
-    created() {}
+    methods: {
+      getsessRankData() {
+        axios.get('/api/integrateMonitor/table.json')
+          .then(res => {
+            res = res.data
+            if (res.ret && res.data) {
+              const data = res.data.flows
+              this.sessRankData = data.sessRank
+            }
+          })
+      }
+    },
+    created() {
+      // 获取会话排行表格数据
+      this.getsessRankData()
+    }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
   .container
+    .indicator
+      margin-top: 20px
+  .container
+    .table-wrapper
     .chart-wrapper
-      margin-top: 18px
+      margin-top: 10px
 </style>
